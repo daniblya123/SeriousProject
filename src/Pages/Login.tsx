898b0ed35ff26e97d340dd547bc2d75814264195
daniblya123 <144435730+daniblya123@users.noreply.github.com>
2024-02-01 09:42:14 +0000

    yes

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,10 +1,52 @@
+import { FormEvent, useState } from "react";
 import "./LoginLayout.css";
+import axios from "axios"
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [responseText, setResponseText] = useState("");
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    setResponseText("");
+    try {
+      const response = await axios.post("http://localhost:5000/login", {
+        username: email,
+        password: pass,
+      });
+      setResponseText(response.data);
+      console.log(response.data)
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setResponseText(error.message);
+      } else {
+        setResponseText(String(error));
+      }
+    }
+  }
+
+
   return (
     <div className="container">
       <div className="inner-container">
         <p>Login </p>
+        return (
+  <>
+  <div className="Login">
+    <div className="LoginForm">
+    <form onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email"/>
+          <label htmlFor="password">Password</label>
+          <input value={pass} onChange={(e) => setPass(e. target.value)} type="password" placeholder="*********" id="password" name="password"/>
+          <button type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+  
+  </>
+  )
       </div>
     </div>
   );
---
+++ src/flask-server.py
@@ -0,0 +1,35 @@
+from bcrypt import checkpw, gensalt, hashpw
+from flask import Flask, request
+from flask_cors import CORS
+import sqlite3
+
+
+  
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route("/login", methods=["POST"])
+
+def login():
+    con = sqlite3.connect('G:\data.db')
+    cu = con.cursor()
+    email = request.json["username"]
+    password = request.json["password"]
+
+    emailQuery = """Select Password From UserData Where Email = ?"""
+    cu.execute(emailQuery, (email,))
+    con.commit()
+    response = cu.fetchall()
+    if len(response) == 0:
+        return "Invalid login.", 200
+    storedPassword = response[0][0]
+    print("Stored PW: " + storedPassword)
+    if password == storedPassword:
+        return "Valid login.", 200
+    
+    else: return "Invalid login.", 200
+
+if __name__ == "__main__":
+    app.run()
+
+
