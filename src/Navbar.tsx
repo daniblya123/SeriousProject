d695ced03c29f7efc4a92195f2cf5c3516d6ed91
daniblya123 <144435730+daniblya123@users.noreply.github.com>
2024-02-14 14:29:44 +0000

    adsad

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,6 +1,7 @@
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 import { useCollapse } from 'react-collapsed';
+import Symbol from "./SHITCOMPANYLOGO.png"
 
 function Navbar() {
   const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
@@ -15,14 +16,18 @@ function Navbar() {
           </li>
         </ul>
         <ul>
-          <li >
-            <NavLink to="#" {...getToggleProps()}>{isExpanded ? 'Login ▽' : 'Login △'}</NavLink>
+          <li >  
+            <NavLink to="#" {...getToggleProps()}>{isExpanded ? 'Login ▽' : 'Login △' } </NavLink>
+          
             <div className="loginContainer" {...getCollapseProps()}>
               <form>
                 <p className="formTitle">Login</p>
                 <div className="loginForm">
+                  <p className="loginText">Username</p>
                 <input type="text" placeholder="Username"/>
+                <p className="loginText">Password</p>
                 <input type="password" placeholder="Password"/>
+                <button className="registerButton">Register</button>
                 </div>
                 <input  className="loginButton" type="submit" placeholder="SUBMIT"/>
               </form>
