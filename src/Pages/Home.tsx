19f316f1013b2e3a6d4a07a07dd65cc5926649f0
daniblya123 <144435730+daniblya123@users.noreply.github.com>
2024-03-08 10:53:03 +0000

    crazychanges

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,7 @@
 import "./HomeLayout.css";
 import Donut from "../Assets/donut.png";
+import Ramen from "../Assets/Ramen.png";
+import Pizza from "../Assets/Pizza.png";
 
 function Home() {
   return (
@@ -7,7 +9,44 @@ function Home() {
       <p className="MenuText">Menu</p>
       <div className="imageRowContainer">
         <div className="imageRow">
-          <h1>sfndushfhusidhfuisdhfushufsdh</h1>
+          <div className="box">
+            <img src={Donut} className="images"></img>
+            <h3>Crazy-Donut</h3>
+          </div>
+          <div className="box">
+            <img src={Ramen} className="images"></img>
+            <h3>Ramen</h3>
+          </div>
+          <div className="box">
+            <img src={Pizza} className="images"></img>
+            <h3>Pep-Pizza</h3>
+          </div>
+        </div>
+      </div>
+      <div className="imageRowContainer">
+        <div className="imageRow">
+          <div className="box">
+            <img src={Donut} className="images"></img>
+          </div>
+          <div className="box">
+            <img src={Donut} className="images"></img>
+          </div>
+          <div className="box">
+            <img src={Donut} className="images"></img>
+          </div>
+        </div>
+      </div>
+      <div className="imageRowContainer">
+        <div className="imageRow">
+          <div className="box">
+            <img src={Donut} className="images"></img>
+          </div>
+          <div className="box">
+            <img src={Donut} className="images"></img>
+          </div>
+          <div className="box">
+            <img src={Donut} className="images"></img>
+          </div>
         </div>
       </div>
     </>
